4aa58e8824c58b3b9a8c6c3490c2d29dd06b982f
郭强 <guoqiang1@letv.com>
2016-03-31 22:33:53 +0800

    add

--- auto_reply/reply.py
+++ auto_reply/reply.py
@@ -13,6 +13,10 @@ from weixin_mp.config import WEIXIN_ID
 def text_reply_xml(to_username, text):
     """构造文本回复的xml
     """
+    print "text_reply_xml.to_username"
+    print to_username
+    print WEIXIN_ID
+    
     xml = u"""
             <xml>
             <ToUserName><![CDATA[%s]]></ToUserName>
