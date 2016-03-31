cc3822353d8989b4a182035c6a6d77afaea932e5
郭强 <guoqiang1@letv.com>
2016-03-31 17:39:39 +0800

    add

--- weixin_mp/urls.py
+++ weixin_mp/urls.py
@@ -15,6 +15,7 @@ urlpatterns = patterns('',
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
 
-    url(r'^weixin/$', 'weixin.views.weixin_main'),
+    #url(r'^weixin/$', 'weixin.views.weixin_main'),
+     url(r'^$', 'weixin.views.weixin_main'),
 
 )
